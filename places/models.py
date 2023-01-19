cc244ab42ebff3017e324614878091d0c0df57f5
Ash2803 <ashot.93@mail.ru>
2023-01-20 01:32:09 +0400

    Setup media files serving

--- places/models.py
+++ places/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField(blank=True)
+    title = models.CharField(max_length=200, verbose_name='Title')
+    description = models.TextField(blank=True, verbose_name='Description')
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(verbose_name='Image', null=True, blank=True, upload_to='images/')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='name', verbose_name='name')
+
+    def __str__(self):
+        return f'{self.id} {self.place.title}'
