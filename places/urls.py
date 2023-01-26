3eb6e5f1aacde388160e5796b3556a024f32f70c
Ash2803 <ashot.93@mail.ru>
2023-01-27 02:08:50 +0400

    Added detail view in json format

--- places/urls.py
+++ places/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from places.views import main
+from places.views import main, detail_place_view
 
 
 urlpatterns = [
     path('', main, name='main'),
+    path('places/<place_id>/', detail_place_view, name='place')
 ]
--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 
 from places.models import Place, Image
 
@@ -8,9 +9,7 @@ def main(request):
         "type": "FeatureCollection",
         "features": []
     }
-
     for place in Place.objects.all():
-        imgages = [image for image in Image.objects.filter(place__id=place.id).values_list('image', flat=True)]
         places_geojson['features'].append(
             {
                 "type": "Feature",
@@ -26,3 +25,18 @@ def main(request):
             }
         )
     return render(request, 'index.html', {'places': places_geojson})
+
+
+def detail_place_view(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    formatted_place = {
+        "title": place.title,
+        "imgs": [image for image in Image.objects.filter(place__id=place.id).values_list('image', flat=True)],
+        "description_short": place.short_description,
+        "description_long": place.long_description,
+        "coordinates": {
+            "lng": place.longitude,
+            "lat": place.latitude
+        }
+    }
+    return JsonResponse(formatted_place, json_dumps_params={'indent': 4, 'ensure_ascii': False})
