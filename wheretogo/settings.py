c823103278e63529d33cf739c700468d720d700e
Ash2803 <ashot.93@mail.ru>
2023-01-28 05:03:34 +0400

    Added production settings

--- wheretogo/settings.py
+++ wheretogo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ddzq!a&zy%+e43twzfhng5cgp(=_g2knn$&f8etdf(z!bq7!(%'
+load_dotenv()
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
@@ -53,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CSRF_COOKIE_SECURE = True
 ROOT_URLCONF = 'wheretogo.urls'
 
 TEMPLATES = [
