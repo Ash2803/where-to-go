ada2ce183675448e6e9b2d1a22e648b87065a415
Ash2803 <ashot.93@mail.ru>
2023-01-29 02:16:29 +0400

    Added unique arguments to the models fields

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,4 @@
+from django.core.management.base import BaseCommand, CommandError
+from places.models import Place
+
+
--- places/models.py
+++ places/models.py
@@ -3,25 +3,26 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Title')
+    title = models.CharField(max_length=200, verbose_name='Title', unique=True)
     long_description = HTMLField(blank=True, verbose_name='Long description')
     short_description = models.TextField(blank=True, verbose_name='Short description')
-    longitude = models.FloatField(null=True)
-    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True, unique=True)
+    latitude = models.FloatField(null=True, unique=True)
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    image = models.ImageField(verbose_name='Image', null=True, blank=True, upload_to='images/')
+    image = models.ImageField(verbose_name='Image', null=True, blank=True, upload_to='images/', unique=True)
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', verbose_name='place')
     id = models.PositiveIntegerField(
         default=0,
         blank=False,
         null=False,
         db_index=True,
-        primary_key=True
+        primary_key=True,
+        unique=True
     )
 
     def __str__(self):
