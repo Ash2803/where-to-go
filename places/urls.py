1e98ee4bf3e5eeeea65b1dcc089265ee7c9ee8a9
Ash2803 <ashot.93@mail.ru>
2023-02-01 18:35:55 +0400

    Fixed urls.py formatting

--- places/urls.py
+++ places/urls.py
@@ -2,11 +2,7 @@ from django.urls import path
 
 from places.views import places_view, detail_place_view
 
-
 urlpatterns = [
     path('', places_view, name='places'),
     path('place/<place_id>/', detail_place_view, name='place')
 ]
-
-
-
